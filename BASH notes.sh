9339941619bb34a5d26019c24f20152ed9e99c03
Alain Bonus <153412582+a-bonus@users.noreply.github.com>
2024-03-05 11:35:43 -0500

    Update BASH notes.sh

--- BASH notes.sh
+++ BASH notes.sh
@@ -1,5 +1,8 @@
 # Bash means bourne again shell, zsh is the updated shell for macOS
 
+#pwd is print working directory 
+'pwd'
+
 #Echo 
 ‘Echo’ # is a print command on terminal 
 
@@ -26,6 +29,10 @@ R = read, w= write, x = execute
 ‘./’ is the same as bash to run a command line script, no space though 
 ‘#!/bin/bash’ write these at the top of the script to let command line know its in bash
 
+#python shebang script header
+#!/usr/bin/env python3
+
+
 #PATH locate using 'which' 
 ‘which’ python3, finds where the PATH is for python3 specifically 
 
@@ -39,4 +46,28 @@ cd -: Changes the current directory to the previous working directory.
 
 # touch 
 'touch' creates a new file in the current directory 
+
+#absolutepath
+/Users/yourusername/Desktop/hello.py 
+to find 'yourusername', run  'whoami' in terminal 
+
+#/
+/ is the root directory, which is the top of the file hierarchy
+
+#cat will show or concatenate files, merging, etc.
+'cat'
+
+#cp is the copy command 
+'cp myfile.txt backup/myfile.txt' #will copy myfile.txt to the 
+# backup directory with the same name.
+'cp -r /myfolder /backup/myfolder' #will copy the entire myfolder 
+#directory to /backup/myfolder, preserving its structure and contents.
+
+# $variable to access a variable 
+
+# read gets user input and stores in the variable 
+'read name' would ask for user input and store it in the name variable
+
+#$1 $2 are positional parameters or positional arguments 
+
 
