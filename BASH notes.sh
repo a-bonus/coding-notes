e404faae2c9ec1f3b1f57156786925da2032de96
Alain Bonus <153412582+a-bonus@users.noreply.github.com>
2024-03-05 10:42:38 -0500

    Create BASH notes.sh
    note update

---
+++ BASH notes.sh
@@ -0,0 +1,38 @@
+# Bash means bourne again shell, zsh is the updated shell for macOS
+
+#Echo 
+‘Echo’ # is a print command on terminal 
+
+#Nano
+‘Nano’ # is a text editor 
+‘Nano’ .sh will create a shell script 
+
+#! Is called a shebang 
+
+#ls list 
+‘Ls’ will list everything in the current directory 
+‘Ls -l’ will show the permissions of the file 
+
+#sleep, pause 
+‘Sleep 3’ will create a pause in a bash script 
+
+#rwx 
+R = read, w= write, x = execute 
+
+#make it executable 
+‘Chmod +x’ adds permission for execution 
+
+#bash shortcut to execute a script 
+‘./’ is the same as bash to run a command line script, no space though 
+‘#!/bin/bash’ write these at the top of the script to let command line know its in bash
+
+#PATH locate using 'which' 
+‘which’ python3, finds where the PATH is for python3 specifically 
+
+# change directory 
+cd /: Changes the current directory to the root directory.
+cd /usr/local: Changes the current directory to /usr/local, which is an absolute path.
+cd documents: Changes the current directory to a subdirectory named documents that is located within the current directory, which is a relative path.
+cd ..: Moves one directory level up (to the parent directory).
+cd ~: Changes the current directory to the home directory of the current user.
+cd -: Changes the current directory to the previous working directory.
